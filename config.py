271d308c7e5c68c9d63777f213513baf75c2828d
Ivan Timofeev <timofeevis89@gmail.com>
2023-10-28 10:11:58 +0300

    pep8 adjustments

--- config.py
+++ config.py
@@ -2,20 +2,19 @@ import os
 import vk_api
 from dotenv import load_dotenv
 from vk_api.longpoll import VkLongPoll
-import sqlalchemy as sa
-from sqlalchemy.orm import sessionmaker
-
 
 load_dotenv()
+
 # token group
 token_group = os.getenv('TOKENAPI')
+
 # token user
 token_user = os.getenv('TOKENUSER')
 
 # Ссылки на токены
 # токен группы
 vk = vk_api.VkApi(token=token_group)
+
 # токен пользователя
 vk2 = vk_api.VkApi(token=token_user)
 longpoll = VkLongPoll(vk)
-
--- db_files/configdb.py
+++ db_files/configdb.py
@@ -1,16 +1,15 @@
 import sqlalchemy as sa
 from sqlalchemy.orm import sessionmaker
 
-
 # DB settings
 DB_NAME: str = 'vkinder'
 user: str = 'postgres'
-password: str = '1209'
+password: str = '6802425'
 DSN: str = f'postgresql://postgres:{password}@localhost:5432/{DB_NAME}'
 echo: bool = False
+
 # переменные model.py
 engine = sa.create_engine(url=DSN, echo=echo)
 vk_url_base = 'https://vk.com/'
 Session = sessionmaker(bind=engine)
 session = Session()
-
